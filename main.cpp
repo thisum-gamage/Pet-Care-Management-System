0248bcc72bd1bd7dbd2c67d5378b30bfeba26281
Thisum Gamage <thisumgmg@gmail.com>
2026-07-12 13:48:11 +0530

    Reformat owner and pet struct definitions for improved readability in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 
 struct owner
-  {
-    string owner_id, first_name, last_name, mobile_num, address;
-    int registered_date;
-  };
-  
+{
+  string owner_id, first_name, last_name, mobile_num, address;
+  int registered_date;
+};
+
+struct pet
+{
+  string pet_id, owner_id, pet_name, pet_type, breed, gender, special_notes;
+  int age;
+};
+
 int main()
 {
-  
+
   return 0;
 }

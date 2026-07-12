058e7d0c94d03a51b79e7f8417d40dd3f241fb67
Thisum Gamage <thisumgmg@gmail.com>
2026-07-12 13:36:24 +0530

    Add initial implementation of owner struct in main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  struct owner
+  {
+    string owner_id, first_name, last_name, mobile_num, address;
+    int registered_date;
+  };
+  return 0;
+}
